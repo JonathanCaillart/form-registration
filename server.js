6993ecad83bf6bafc622028dfc1b6d6c878b3266
CJ <69832606+JonathanCaillart@users.noreply.github.com>
2020-10-20 13:55:51 +1100

    commit

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static('public')); 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -14,37 +12,25 @@ const connection = mysql.createConnection({
   user: "root",
   password: "secret"
 });
-
 app.post("/register", function(req, res) {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
   const password = req.body.password;
-
   const user = {
     firstName: firstName,
     lastName: lastName,
     email: email,
     password: password,
   };
-
-  connection.connect(function(err) {
-    if (err) throw err;
-
-    connection.query("INSERT INTO users SET ?", user, function(err) {
-      if (err) throw err;
-
-      return res.redirect("success.html");
-    });
-  });
+  connection.query("INSERT INTO users SET ?", user);
+  return res.redirect("success.html");
 });
-
 app
   .get("/", function(req, res) {
     return res.redirect("index.html");
   })
   .listen(3000);
-
 console.log("server listening at port 3000");
 
 function Validate() {
